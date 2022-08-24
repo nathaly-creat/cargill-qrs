e438c47e9c8b687c8d113c735792c1bce760b400
Karen Alarcón <akareny.10@gmail.com>
2022-08-24 17:34:23 -0500

    librerias y ruteo para sugerencias

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {NotAccess} from '../components/Shared/NotAccess.jsx'
-import { HomePrueba } from '../components/Home/HomePrueba.jsx';
-
+// import { HomePrueba } from '../components/Home/HomePrueba.jsx';
+import {Suggestions}  from '../components/Suggestions/Admin'
 
 // ROUTER PRINCIPAL
 export default function AppRouter() {
@@ -10,7 +10,8 @@ export default function AppRouter() {
       <BrowserRouter>
         <Routes>
           <Route path='/*' element={<NotAccess />} data-testid='change-views-NotFound'/>
-          <Route path='/' element={<HomePrueba />} />
+          {/* <Route path='/' element={<HomePrueba />} /> */}
+          <Route path='/' element={<Suggestions/>} />
         </Routes>
       </BrowserRouter>
     );
